14ea05eb05633380d0f64df67148d9b4c1acec9d
wilsaj <wilson.andrew.j+github@gmail.com>
2011-09-07 17:41:23 -0500

    add some logic to get virtualenvs working with Ubuntu and/or EPD

--- custom/wilsaj.zsh
+++ custom/wilsaj.zsh
@@ -1,23 +1,41 @@
 # My custom zsh stuff
 
+# special cases for Ubuntu
+if [[ -n `uname -a | grep Ubuntu` ]]; then
+      VIRTUALENV_DIR=/usr/local/bin/
+   else
+      VIRTUALENV_DIR=/usr/bin/
+   fi
+
+# make virtualenvs work with EPD
+if [[ -d /usr/local/share/epd ]]; then
+      PYTHON_DIR=/usr/local/share/epd/
+      PYTHON=${PYTHON_DIR}/bin/python
+      PYTHONPATH=${PYTHON_DIR}/bin/python
+      VIRTUALENV_DIR=/usr/local/share/epd/bin/
+      export PATH=${PYTHON_DIR}/bin/:${PATH}
+   else
+      PYTHON=python2
+      PYTHONPATH=/usr/bin/python2
+   fi
+
+
 # ls aliases
 alias la='ls -a'
 alias lf='ls -FA'
 alias ll='ls -lAF'
-
 
 # editor
 export EDITOR='emacsclient'
 
 
 # Make python2 the explicit python of choice (for now)
-export PYTHON='python2'
-export PYTHONPATH='/usr/bin/python2'
+export PYTHON=${PYTHON}
+export PYTHONPATH=${PYTHONPATH}
 
 
 # And for pymacs
-export PYMACS_PYTHON='python2'
-
+export PYMACS_PYTHON=${PYTHON}
 
 # Don't let pip install anything if not in an active virtualenv
 export PIP_REQUIRE_VIRTUALENV=true
@@ -25,9 +43,9 @@ export PIP_RESPECT_VIRTUALENV=true
 
 
 # virtualenvwrapper config
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2
+export VIRTUALENVWRAPPER_PYTHON=${PYTHONPATH}
 export WORKON_HOME=$HOME/.virtualenvs
-source /usr/bin/virtualenvwrapper.sh
+source ${VIRTUALENV_DIR}/virtualenvwrapper.sh
 
 
 # make ipython configuration directory
