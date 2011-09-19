f862670ea168a8ebd454e56e9deb8fddaa08d308
wilsaj <wilson.andrew.j+github@gmail.com>
2011-09-19 11:53:48 -0500

    wrap git with hub if hub is installed

--- custom/wilsaj.zsh
+++ custom/wilsaj.zsh
@@ -2,9 +2,9 @@
 
 # special cases for Ubuntu
 if [[ -n `uname -a | grep Ubuntu` ]]; then
-      VIRTUALENV_DIR=/usr/local/bin/
+      export VIRTUALENV_DIR=/usr/local/bin/
    else
-      VIRTUALENV_DIR=/usr/bin/
+      export VIRTUALENV_DIR=/usr/bin/
    fi
 
 # make virtualenvs work with EPD
@@ -12,7 +12,7 @@ if [[ -d /usr/local/share/epd ]]; then
       PYTHON_DIR=/usr/local/share/epd/
       PYTHON=${PYTHON_DIR}/bin/python
       PYTHONPATH=${PYTHON_DIR}/bin/python
-      VIRTUALENV_DIR=/usr/local/share/epd/bin/
+      export VIRTUALENV_DIR=/usr/local/share/epd/bin/
       export PATH=${PYTHON_DIR}/bin/:${PATH}
    else
       PYTHON=python2
@@ -24,6 +24,9 @@ if [[ -d /usr/local/share/epd ]]; then
 alias la='ls -a'
 alias lf='ls -FA'
 alias ll='ls -lAF'
+
+# use hub as a wrapper for git if hub is installed
+[[ -x $(whence -p hub) ]] && function git(){hub "$@"}
 
 # editor
 export EDITOR='emacsclient'
